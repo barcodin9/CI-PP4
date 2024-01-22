b0b66bd7eb09483d93a6a492c08ad1660ebbc385
barcodin9 <barcodingss@gmail.com>
2024-01-22 22:34:58 +0000

    Add basic search function on home page with JS

--- js/script.mjs
+++ js/script.mjs
@@ -1,13 +1,19 @@
-import { error } from "console"
-import fetch from "node-fetch"
-async function test () {
-    try {
-        const whosnews = await fetch ('https://api.worldnewsapi.com/search-news?api-key=903a1fea0c8b4e7c996d81c552b613b5&text=apple').then(res => res.json())
-        console.log(whosnews)
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
+// import fetch from "node-fetch"
+// async function test () {
+//     try {
+//         const whosnews = await fetch ('https://api.worldnewsapi.com/search-news?api-key=903a1fea0c8b4e7c996d81c552b613b5&text=apple&language=en&number=10').then(res => res.json())
+//         console.log(whosnews)
+//     }
+//     catch (err) {
+//         console.log(err)
+//     }
+// }
 
-test()
+const searchInput = document.getElementById('searchinput')
+const button = document.getElementById('searchbutton')
+
+button.addEventListener('click', (e) => {
+    console.log(searchInput.value)
+})
+
+// test()
