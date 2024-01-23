d820d4ba144dc5577da64c27c944b456fcbce939
barcodin9 <barcodingss@gmail.com>
2024-01-23 13:17:05 +0000

    Add function to display API content as a card (only displays blank card yet)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 async function newsSearch (userSearch) {
     try {
-        const whosnews = await fetch (`https://api.worldnewsapi.com/search-news?api-key=903a1fea0c8b4e7c996d81c552b613b5&text=${userSearch}&language=en&number=5`).then(res => res.json())
-        console.log(whosnews)
+        const response = await fetch(`https://api.worldnewsapi.com/search-news?api-key=903a1fea0c8b4e7c996d81c552b613b5&text=${userSearch}&language=en&number=3`);
+        const whosnews = await response.json();
+        console.log("API Response:", whosnews);
+        displayNews(whosnews);
     }
     catch (err) {
         console.log(err)
     }
+}
+
+function displayNews(newsData) {
+    const newsContainer = document.getElementById('newsContainer');
+    newsContainer.innerHTML = '';
+
+    newsData.news.forEach(article => {
+        const card = document.createElement('div')
+            card.className = 'card'
+            card.innterHTML = `
+            <h3>${article.title}</h3>
+            <p>${article.text}</p> 
+            <a href="${article.url}" target="_blank">Read More!</a>
+            `
+        newsContainer.appendChild(card)
+    })
 }
 
 const searchInput = document.getElementById('searchinput')
@@ -15,3 +33,4 @@ button.addEventListener('click', async() => {
     const response = await newsSearch(searchInput.value)
     console.log(response)
 });
+
