41281fb2b161e966fdca03f05d9a18ad0567c753
barcodin9 <barcodingss@gmail.com>
2024-01-26 00:52:06 +0000

    User creation post to Database

--- backend/db/views.py
+++ backend/db/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
 
 # Create your views here.
 def home(request): 
@@ -11,7 +13,21 @@ def login(request):
     return render(request, 'login.html')
 
 def signup(request): 
-    return render(request, 'signup.html')
+    print(request.POST)
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            print('valid')
+            form.save()
+            return redirect('home')
+        else:
+            print(form.errors)
+    else:
+        print('test')
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
 def contact(request): 
     return render(request, 'contact.html')
+
+
