1f5f890f04127fe8156da79bf43f1c169e4b0789
barcodin9 <barcodingss@gmail.com>
2024-01-26 08:46:50 +0000

    Configure Heroku database settings

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,7 +87,10 @@ DATABASES = {
        'PASSWORD': 'password',
        'HOST': 'localhost',
        'PORT': '',
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+
     }
+    
 }
 
 
